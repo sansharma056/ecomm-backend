9b1a93827718aa215ef193c8867025b785ce8454
Sankalp Sharma <sansharma056@gmail.com>
2022-05-22 17:07:29 +0530

    Added handler to route

--- src/resources/product/product.router.ts
+++ src/resources/product/product.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { create, getAll, getById } from "./product.controller";
+import { create, deleteById, getAll, getById } from "./product.controller";
 
 const router = Router();
 
 router.post("/", create);
-router.delete("/");
+router.delete("/", deleteById);
 router.get("/", getAll);
 router.get("/:id", getById);
 
